515ad2eda31bd7cef9c433d86bd69c6151bd7abb
Greg Fleurdépine <77107201+trueGFL@users.noreply.github.com>
2021-01-07 18:56:16 +0100

    Create spiraling_colored_squares.py
    Script drawing colored spiraling squares. First try at functions.

---
+++ spiraling_colored_squares.py
@@ -0,0 +1,26 @@
+#spiraling_colored_squares
+
+import turtle
+
+def draw_multicolor_square(t, sz):
+    """Make turtle t draw a multi-color square of sz."""
+    for i in ["red", "purple", "hotpink", "blue"]:
+        t.color(i)
+        t.forward(sz)
+        t.left(90)
+
+wn = turtle.Screen()        # Set up the window and its attributes
+wn.bgcolor("black")
+
+tess = turtle.Turtle()      # Create tess and set some attributes
+tess.pensize(3)
+tess.speed(200)
+
+size = 20                   # Size of the smallest square
+for i in range(16):
+    draw_multicolor_square(tess, size)
+    size = size + 10        # Increase the size for next time
+    tess.forward(10)        # Move tess along a little
+    tess.right(18)          #    and give her some turn
+
+wn.mainloop()
