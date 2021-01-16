2a7b723df9fc14fc6fed80f5d66321894187e2c8
Greg Fleurdépine <77107201+trueGFL@users.noreply.github.com>
2021-01-16 12:26:22 +0100

    Create area_of_circle.py

---
+++ area_of_circle.py
@@ -0,0 +1,11 @@
+# Write a function area_of_circle(r) which returns the area of a circle of radius r.
+
+
+import math
+
+r = int(input("r?"))
+def area_of_circle(A):
+    a =  math.pi * (r ** r)
+    print(a)
+
+area_of_circle(r)
